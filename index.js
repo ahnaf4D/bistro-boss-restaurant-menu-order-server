6cb9c0b67efd43171b1c3ffd01ccf3d14102d5d9
ahnaf4d <ahnaftahmid802@gmail.com>
2024-05-20 17:54:21 +0600

    mongodb setup

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -5,7 +6,27 @@ const port = process.env.PORT || 3000;
 // middlewares
 app.use(express.json());
 app.use(cors());
-// routes
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrua0aj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('bistro boss restaurant here!!!');
 });
